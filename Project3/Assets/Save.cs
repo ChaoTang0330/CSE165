3a219072b22b6aaa539cb0ca7e2e17c513423fcb
ChaoTang0330 <46731373+ChaoTang0330@users.noreply.github.com>
2021-02-20 20:21:24 -0800

    Project3

---
+++ Project3/Assets/Save.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class Save : MonoBehaviour
+{
+    public Renderer cube;
+
+    public Transform chairs;
+    public Transform desks;
+
+    public string fileName = "Classroom.info";
+
+    private string filePath;
+
+    //private Material cubeMaterial;
+    private bool isSelected = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //cubeMaterial = cube.material;
+        filePath = Path.Combine(Application.persistentDataPath, fileName);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "VirtualHand")
+        {
+            SetIntersect();
+        }
+        Debug.Log("Collider: " + other.gameObject.name);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "VirtualHand")
+        {
+            if (OVRInput.Get(OVRInput.Button.SecondaryIndexTrigger))
+            {
+                SetSelect();
+            }
+            else
+            {
+                SetIntersect();
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "VirtualHand")
+        {
+            SetSilent();
+        }
+            
+    }
+
+    public void SetIntersect()
+    {
+        isSelected = false;
+        cube.material.SetColor("_EmissionColor", Color.white);
+    }
+
+    public void SetSelect()
+    {
+        if(!isSelected)
+        {
+            isSelected = true;
+            cube.material.SetColor("_EmissionColor", Color.red);
+            SaveInfo();
+        }
+    }
+
+    public void SetSilent()
+    {
+        isSelected = false;
+        cube.material.SetColor("_EmissionColor", Color.black);
+    }
+
+    private void SaveInfo()
+    {
+        StreamWriter sw;
+
+        try
+        {
+            sw = File.CreateText(filePath);
+        }
+        catch (System.IO.IOException e)
+        {
+            Debug.Log("Save: " + e.Message);
+            return;
+        }
+
+        //Debug.Log("Save: Chairs " + chairs.childCount.ToString());
+        sw.WriteLine(chairs.childCount);
+        foreach (Transform child in chairs)
+        {
+            Vector3 locPos = child.localPosition;
+            Vector3 locRot = child.localEulerAngles;
+            sw.WriteLine("{0} {1} {2} {3} {4} {5}",
+                locPos.x, locPos.y, locPos.z,
+                locRot.x, locRot.y, locRot.z);
+        }
+
+        //Debug.Log("Save: Desks " + chairs.childCount.ToString());
+        sw.WriteLine(desks.childCount);
+        foreach (Transform child in desks)
+        {
+            Vector3 locPos = child.localPosition;
+            Vector3 locRot = child.localEulerAngles;
+            sw.WriteLine("{0} {1} {2} {3} {4} {5}",
+                locPos.x, locPos.y, locPos.z,
+                locRot.x, locRot.y, locRot.z);
+        }
+
+        sw.Close();
+        sw.Dispose();
+    }
+}
---
+++ Project3/Assets/VirturalHand.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VirturalHand : MonoBehaviour
+{
+    public ControlFurniture cf;
+    public Transform torso;
+
+    public float GoGoD;
+    public float GoGoAlpha;
+
+    private bool GoGoMode;
+
+    private Transform rightController;
+
+    void Awake()
+    {
+        rightController = transform.parent.transform;
+        GoGoMode = cf.GoGoMode;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (GoGoMode)
+        {
+            transform.localPosition = new Vector3(0.0f, 0.0f, 0.0f);
+            //transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GoGoMode)
+        {
+            Vector3 handPos = rightController.position - torso.position;
+            float length = handPos.magnitude;
+            if (length > GoGoD)
+            {
+                length += GoGoAlpha * (length - GoGoD) * (length - GoGoD);
+                handPos = handPos * length / handPos.magnitude + torso.position;
+                transform.localPosition = rightController.InverseTransformPoint(handPos);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        cf.EnterSphereIntersection(other);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        cf.ExitSphereIntersection(other);
+    }
+}
